6f37acb27b4ce5c640551e95dc3b4c4588449e0f
lymarrie <lucmarrie1@gmail.com>
2021-10-05 22:20:00 -0400

    professionals page work

---
+++ tailwind.config.js
@@ -0,0 +1,43 @@
+module.exports = {
+  mode: "jit",
+  purge: [
+    "./partials/**/*.hbs",
+    "./pages/**/*.hbs",
+    "./templates/**/*.hbs",
+    "./src/**/*.js",
+    "./src/**/*.ts",
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    screens: {
+      'xs': '475px',
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
+    extend: {
+      colors: {
+        "ach-blue": "#004590",
+        "ach-hover-blue": "#23527c",
+        "ach-green": "green",
+        "ach-light-green": "#339933",
+        "ach-gray": "#ECECEC"
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
